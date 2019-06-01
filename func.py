36e0d9cd98477c6352182a209de36cd274fe0943
ILYA PEREVOZKIN <iperevozkin@gmail.com>
2019-06-01 23:38:34 +0500

    v1.1

--- func.py
+++ func.py
@@ -1,30 +1,27 @@
-from  tkinter import *
 import os
-from tkinter import filedialog
+from tkinter import filedialog, END
 #######################################################
-# функция выбора директории и записи её в переменную
 
 
-def searchDirectory():
-    directory = filedialog.askdirectory()
-    handle = open('directory.txt', 'w')
-    handle.write(directory)
-    handle.close()
-    pass
+# функция открытия файлов в LISTBOX
+def open(directory, massivData, listbox1):
+    massivData.clear()
+    listbox1.delete(0, 'end')
+    massivData.extend(dirLoop(directory))
+    addBox(massivData, listbox1)
+
+
+# функция выбора пути
+def searchDirectory(directory, textDir):
+     textDir.delete(1.0, END)
+     directory.clear()
+     directory.insert(0, filedialog.askdirectory())
+     textDir.insert(1.0, directory)
+
 
 # функция - обход директории с глубокой рекурсией на поиск .pdf
-
-
-def dirLoop():
-    # контейнер list
-    directory = []
-    handle = open('directory.txt', 'r')
+def dirLoop(directory):
     massiv = []
-    for line in handle.readlines():
-        directory.append(line)
-    handle.close()
-    print(directory)
-    # рекурсивный обход директории на поиск файлов .pdf
     for root, dirs, files in os.walk(directory[0]):
         for file in files:
             if file.endswith(".pdf"):
@@ -32,4 +29,20 @@ def dirLoop():
                 massiv.append([(os.path.join(file)), root])
     return massiv
 
-#############################################################################
+
+# функция запуска выбранного файла
+def fileOpen(listbox1, massivData):
+    # индекс выделенного файла listbox (список) curselection функция
+    sel = list(listbox1.curselection())
+    ''' читает дирикторию с файлами и делает кортеж из списка произведений [0],
+     [1] - полный путь до папки'''
+    os.startfile(os.path.join(massivData[sel[0]][1], massivData[sel[0]][0]))
+# добавление списка в LIstbox
+
+
+def addBox(massivData, listbox1):
+    for i in massivData:
+        listbox1.insert(0, i[0])
+    return listbox1
+
+###############################################################
--- main.py
+++ main.py
@@ -1,66 +1,34 @@
 # импорт Tkinter
-from tkinter import *
-# функция dir_loop (перебирает файлы в указанной директории)
-from func import dirLoop, searchDirectory
-# библиотека системы
-from os import startfile, path
+import tkinter as tk
+# три функции ВЫБОР ДИРЕКТОРИИ, ОТКРЫТИЕ СПИСКА ФАЙЛОВ, ОТКРЫТИЕ ФАЙЛА
+from func import searchDirectory, open, fileOpen
 ##############################################################################
+''' переменные в которых хранятся переменные
+DIRECTORY - выбранный путь, MASSIVDATA - список файлов.'''
 
+directory = ['путь до файла']
+massivData = []
 ##############################################################################
-# функция запускающая dirLoop -> searchDirectory, получает путь директории,
-# потом парсит директории, добавляет список в listbox
-
-
-def outListing():
-    directory = searchDirectory()
-    listing = dirLoop()
-    for i in listing:
-        listbox1.insert(END, i[0])
-    pass
-
-
-# функция запуска выбранного файла
-def fileSelection():
-    # индекс выделенного файла listbox (список) curselection функция
-    selection = list(listbox1.curselection())
-    ''' читает дирикторию с файлами и делает кортеж из списка произведений [0],
-     [1] - полный путь до папки
-    запуск файла | selection - функция'''
-    startfile(path.join(listing[selection[0]][1], listing[selection[0]][0]))
-
 # Tkinter init
-root = Tk()
+root = tk.Tk()
 # Title
 root.title('PDF_reader')
 # размер окна
 root.geometry('900x600+200+100')
-# init Frame
-#frame = Frame()
-#frame.grid(side = BOTTOM, expand = True)
 # лист списка
-listbox1 = Listbox(root, height=60, width=140)
+listbox1 = tk.Listbox(root, height=20, width=75, font='Arial 15')
 listbox1.grid(row=1, column=0, rowspan=4, columnspan=5, padx=30)
 # текст пути
-textDir = Text(height=1, width=21, font='Arial 15 bold')
-textDir.grid(row=0, column=1)
-
+textDir = tk.Text(height=1, width=35, font='Arial 15')
+textDir.grid(row=0, column=2)
+textDir.insert(1.0, directory)
 
 # init всех Button
-button1 = Button(text="Open file", width=50, command=fileSelection)
-button1.grid()
-button2 = Button(text="output")
-button2.grid(row=0, column=4, pady=10, padx=10)
-button3 = Button(text="directory selection", command=outListing)
-button3.grid(row=0, column=0, pady=10, padx=10)
-button4 = Button(text="Add", width=50)
-button4.grid()
-button5 = Button(text="big", width=50)
-button5.grid()
-button6 = Button(text="class", width=50)
-button6.grid()
-button7 = Button(text="Add", width=50)
-button7.grid()
-button8 = Button(text="Add", width=50)
-button8.grid()
+button1 = tk.Button(text="Open file", command= lambda: fileOpen(listbox1, massivData))
+button1.grid(pady=10, padx=10)
+button2 = tk.Button(text="open", command= lambda: open(directory, massivData, listbox1))
+button2.grid(row=0, column=1, pady=10, padx=0)
+button3 = tk.Button(text="dir selection", command= lambda: searchDirectory(directory, textDir))
+button3.grid(row=0, column=0)
 
-root.mainloop()
+root.tk.mainloop()
