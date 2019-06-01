e132c4c142d70157336e12ae66a0f90d7d8deca7
ILYA PEREVOZKIN <iperevozkin@gmail.com>
2019-06-02 00:06:58 +0500

    remove file

--- runFileSelection.py
+++
@@ -1,6 +0,0 @@
-# функция запуска выбранного файла
-def fileSelection():
-    # индекс выделенного файла listbox (список) curselection функция
-    selection = list(listbox1.curselection())
-    # запуск файла | selection - функция
-    os.startfile(os.path.join(listing[selection[0]][1], listing[selection[0]][0]))
--- searchDirectory.py
+++
@@ -1,6 +0,0 @@
-from  tkinter import *
-from tkinter import filedialog
-# функция выбора директории и записи её в переменную
-def searchDirectory():
-    directory = filedialog.askdirectory()
-    return directory
